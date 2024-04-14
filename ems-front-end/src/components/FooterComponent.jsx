586ab830b456567b1c632ab2148a8cfcff531c12
Sethu <sethuserge@gmail.com>
2024-04-14 20:37:26 +0200

    All CRUD functionality

---
+++ ems-front-end/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FooterComponent(props) {
+    return (
+        <div>
+            <footer className="footer">
+                <span>All rights reserved 2024 by Sethu</span>
+            </footer>
+        </div>
+    );
+}
+
+export default FooterComponent;
---
+++ ems-front-end/src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from "react-dom";
+
+const HeaderComponent = () => {
+    return(
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="https://www.javaguides.net">Employee Management System</a>
+            </nav>
+        </div>
+    )
+}
+export default HeaderComponent;
